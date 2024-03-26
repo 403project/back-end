7bd321af3de93243e314994d78804c4052ff560c
ldw <ldw3097@gmail.com>
2024-03-26 17:43:01 +0900

    spring init

---
+++ src/main/java/byulbyul/byulbyulpoll/ByulbyulpollApplication.java
@@ -0,0 +1,13 @@
+package byulbyul.byulbyulpoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByulbyulpollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByulbyulpollApplication.class, args);
+	}
+
+}
