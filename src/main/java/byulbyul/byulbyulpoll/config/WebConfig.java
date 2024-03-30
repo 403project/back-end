30f75c35939fb144ce1312e7dd9b25e05d3e180f
ldw <ldw3097@gmail.com>
2024-03-30 16:56:23 +0900

    chore: cors 설정 변경

--- src/main/java/byulbyul/byulbyulpoll/config/WebConfig.java
+++ src/main/java/byulbyul/byulbyulpoll/config/WebConfig.java
@@ -11,9 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Authorization", "Content-Type")
-                .exposedHeaders("Custom-Header")
-                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
